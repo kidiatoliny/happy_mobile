bac9d1bffc7b784cd877028bc55d80cfd26ed84e
kid <kidiatoliny@gmail.com>
2020-10-19 00:29:36 -0100

    create register orphanages

--- App.tsx
+++ App.tsx
@@ -1,14 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
+
 import React from 'react';
-import { StyleSheet, View, Dimensions,Text, TouchableOpacity } from 'react-native';
-import  MapView, { Marker, Callout, PROVIDER_GOOGLE } from 'react-native-maps'
-import mapMarker from "./src/images/map_marker.png";
-import { Feather} from '@expo/vector-icons'
+
 import {useFonts} from 'expo-font'
-import {Nunito_600SemiBold, Nunito_700Bold, Nunito_800ExtraBold } from '@expo-google-fonts/nunito'
-
+import {
+  Nunito_600SemiBold,
+   Nunito_700Bold,
+   Nunito_800ExtraBold
+  } from '@expo-google-fonts/nunito'
+import Routes from './src/routes';
 export default function App() {
-
   const [fontsLoaded] = useFonts({
     Nunito_600SemiBold,
     Nunito_700Bold,
@@ -18,99 +18,7 @@ export default function App() {
   if(!fontsLoaded){
     return null
   }
-  return (
-    <View style={styles.container}>
-      <MapView style={styles.map}
-      provider={ PROVIDER_GOOGLE }
-      initialRegion={{
-        latitude:16.8879404,
-        longitude:-24.9847277,
-        latitudeDelta:0.008,
-        longitudeDelta:0.008
-       }}>
-          <Marker
-          icon={mapMarker}
-          calloutAnchor={{
-            x:2.3,
-            y:0.8,
-           }}
-          coordinate={{
-            latitude:16.8879404,
-            longitude:-24.9847277,
-           }}
-          >
-            <Callout tooltip onPress={()=>{ alert('Lar das meninas')}}>
-              <View style={styles.calloutContainer}>
-               <Text style={styles.calloutText}> Lar das meninas </Text>
-              </View>
-            </Callout>
-
-          </Marker>
-       </MapView>
-       <View style={styles.footer}>
-         <Text style={styles.footerText}> 2 orfanatos encontrados</Text>
-         <TouchableOpacity style={styles.createOrphanageButton} onPress={()=>{}}>
-           <Feather name='plus' size={20} color='#fff'/>
-
-         </TouchableOpacity>
-       </View>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  map:{
-    width:Dimensions.get('window').width,
-    height:Dimensions.get('window').height,
-  },
-  calloutContainer:{
-    width:160,
-    height:46,
-    paddingHorizontal:16,
-    backgroundColor:'rgba(255,255,255,0.8)',
-    borderRadius:16,
-    justifyContent:'center',
-  },
-  calloutText:{
-      color:'#0089a5',
-      fontSize:14,
-      fontFamily:'Nunito_700Bold'
-  },
-
-  footer:{
-    position:'absolute',
-    left:24,
-    right:24,
-    bottom:32,
-    backgroundColor:'#fff',
-    borderRadius:20,
-    paddingLeft:24,
-    flexDirection:'row',
-    justifyContent:'space-between',
-    alignItems:'center',
-    elevation:3
-
-
-  },
-  footerText:{
-    color:'#8fa7b3',
-    fontFamily:'Nunito_700Bold'
-  },
-  createOrphanageButton:{
-    width:56,
-    height:56,
-    backgroundColor:'#15c3d6',
-    borderRadius:20,
-    justifyContent:'center',
-    alignItems:'center'
-  }
-
-
-});
+return (
+  <Routes/>
+)
+};
---
+++ src/routes.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack'
+
+const { Navigator, Screen }=createStackNavigator()
+import OrphanagesMap from './pages/OrphanagesMap'
+import OrphanagesDetails from './pages/OrphanagesDetails';
+import SelectMapPosition from './pages/CreateOrphanages/SelectMapPosition';
+import OrphanageData from './pages/CreateOrphanages/OrphanageData';
+import Header from './components/Header'
+
+export default function Routes() {
+  return (
+    <NavigationContainer>
+      <Navigator screenOptions={{
+        headerShown:false,
+        cardStyle:{ backgroundColor:'#f2f3f5'}
+        }}>
+        <Screen name='Orphanages Map' component={OrphanagesMap}/>
+        <Screen
+          name='Orphanages Details'
+          component={OrphanagesDetails}
+          options={{
+            headerShown:true,
+            header:()=><Header showCancel={false} title=" Detalhes do Orfanato"/>
+          }}
+          />
+        <Screen
+          name='Select Map Position'
+          component={SelectMapPosition}
+            options={{
+              headerShown:true,
+              header:()=><Header title="Selecionar Posição no Mapa"/>
+            }}
+          />
+        <Screen
+          name='Orphanage Data'
+          component={OrphanageData}
+          options={{
+            headerShown:true,
+            header:()=><Header title=" Dados do Orfanato"/>
+          }}
+        />
+      </Navigator>
+    </NavigationContainer>
+
+  )
+}
